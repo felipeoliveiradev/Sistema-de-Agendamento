f4a27fbca508fae5a465e788166e90025abe6abc
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2019-02-24 09:59:17 -0300

    commit inicial com erro no upload

---
+++ src/database/migrations/20190222012426-create-users.js
@@ -0,0 +1,47 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING
+      },
+      avatar: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      password_hash: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      provider: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    })
+  },
+  down: (queryInterface, DataTypes) => {
+    return queryInterface.dropTable('users')
+  }
+}
